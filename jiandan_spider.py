81eedfa5286d428e7878cb31ef01315844697721
Anakin Wang <wangyouyuan@sjtu.edu.cn>
2017-06-08 10:41:34 +0800

    Add files via upload
    抓取煎蛋网上的图片

---
+++ jiandan_spider.py
@@ -0,0 +1,53 @@
+# coding:utf-8
+
+from bs4 import BeautifulSoup
+import requests
+from urllib import urlretrieve
+import os
+import threading
+
+numMutex = threading.Lock()
+g_threadNum = 0
+
+def downloadImg(url):
+    html = requests.get(url)
+    bsObj = BeautifulSoup(html.text, 'html.parser')
+    if not os.path.exists('jiandan/'):
+        os.mkdir('jiandan/')
+    for i in bsObj.findAll('img'):
+        if i['src'].startswith('//wx'):
+            #下载远程数据
+            print '%s is downloading.....' % i['src'].split('/')[-1]
+            urlretrieve('http:' + i['src'], 'jiandan/' + i['src'].split('/')[-1])
+            print 'completed!'
+    print 'The images download completed!'
+
+class MyThread(threading.Thread):
+    def __init__(self, url):
+        self.url = url
+        threading.Thread.__init__(self)
+
+    def run(self):
+        numMutex.acquire()
+        downloadImg(self.url)
+        global g_threadNum
+       # print('关闭线程',self.url)
+        g_threadNum -= 1
+        numMutex.release()
+
+def webspider():
+    start = int(raw_input('请输入开始页数：'))
+    end = int(raw_input('请输入结束页数：'))
+    urlPool = ["http://jandan.net/ooxx/page-{}#comments".format(str(i)) for i in range(start, end)]
+    global g_threadNum
+    threadLimit = 5  #使用5个线程
+    while urlPool != []:
+        if g_threadNum < threadLimit:
+            newUrl = urlPool.pop()
+            g_threadNum += 1
+            newThread = MyThread(newUrl)
+            #print('开启一个线程：', newUrl)
+            newThread.start()
+
+if __name__ == '__main__':
+    webspider()
